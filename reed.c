6d39abf6f111bacb30d246e409e59804cef96439
jjinkou2 <jjinkou2@yahoo.fr>
2020-05-10 01:56:12 +0200

    it works

--- reed.c
+++ reed.c
@@ -14,6 +14,10 @@
 #define BKSPACE 8
 #define CLR 12
 
+// ---- constant serial for terminal ---------
+#define TERM_X0 10
+#define TERM_Y0 2
+
 #define ON 22
 #define LCD_RXPIN 24
 #define LCD_TXPIN 24
@@ -22,15 +26,13 @@
 // --- define some value for reed grid ----
 #define reed_start_Pin 8
 #define reed_end_Pin   23
-// ligne en Pins pairs (start=8) et column sur Pins impairs
-#define Columns           0b1010101010101010
-#define Rows              0b0101010101010101
-#define Dir_RowOut_ColIn  0b0101010101010101 // Output = 1 input = 0
-#define RowsHigh          0b0101010101010101
-//#define RowsHigh          0b0101010101010101
+// colonnes en Pins pairs (start=8) et lignes sur Pins impairs
+#define Columns           0x5555 //0b0101010101010101
+#define Rows              0xAAAA //0b1010101010101010
+#define Dir_RowOut_ColIn  0xAAAA // Output = 1 input = 0
+#define RowsHigh          0xAAAA 
 
-
-#define RowsLow           0b0000000000000000
+#define RowsLow           0
 
 
 
@@ -54,8 +56,8 @@
 
 
 // --- Pin Row Col ----
-#define Row1  reed_start_Pin
-#define Row8  reed_end_Pin - 1
+#define COL_A  reed_start_Pin
+#define ROW_1  (reed_start_Pin+1)
 
 //
 // ------ Global Variables and Objects ------
@@ -74,15 +76,13 @@ unsigned char reversebit(unsigned char b) {
 // récupere un bit sur 2 en partant de la droite et du 2eme 
 // ex  si v = 0b11110010
 // retour = 0b1101
-int getcolpinval(int v)
-
+int getcolpinval(unsigned int v)
 {
-    int d,i;
+    unsigned int d,i;
     // efface le dernier bit a droite
-    v>>=1;
+    //v>>=1;
     // on passe de 2 en 2 vers la gauche (on shift v vers la droite)
-    for (d = 0; v; v >>= 2, i++)
-    {
+    for (d = 0; v; v >>= 2, i++) {
       // on accumule le résultat dans d
         d = d | (v&1) << i ;
     }
@@ -95,14 +95,15 @@ int getcolpinval(int v)
 // Si la case est vide =====> O
 void displayrow(int n)
 {
-    char row[16]="XXXXXXXX\0";
+    char row[16]="........\0";
        
      for (int i=0;n; i++) {
-        if (n & 1)  row[i]='O';
+        if (n & 1)  row[i]='.';
         else        row[i]='X';
         n >>= 1;
     }
-    printf("\t%s\n",row);
+//    printf("\t%s\n",row);
+    printf("%s",row);
 }
 
 
@@ -112,31 +113,42 @@ int main()
 {
   // Mettre les lignes en direction output et les colonnes en input
   set_directions(reed_end_Pin,reed_start_Pin,Dir_RowOut_ColIn);
+  pause(500);
+
   // mettre les lignes en High
   set_outputs(reed_end_Pin,reed_start_Pin,RowsHigh);
+  pause(500);
     
-  int pinvalues = 0;  // recupere les valeurs des pins
+  unsigned int pinvalues = 0;  // recupere les valeurs des pins
   int rowpin    = 0;  // pins des lignes
-  pause(100);
-  for (int r = 0; r<8; r++){
-     // Mettre la ligne a 0 
-     rowpin=Row1+r*2;
-     set_output(rowpin,0);
-      
-     // récupère les valeurs des colonnes
-     pinvalues = get_states(reed_end_Pin,reed_start_Pin);
-     
-     
-     //pinvalues = 0b1111001010110010;
+  int d = 0;
+  char test[255];
+  for(;;){
+    //pause(5000);
+    term_cmd(CRSRXY, TERM_X0, TERM_Y0);
 
-     int d = getcolpinval(pinvalues);
-     //printf("col=%d\n",d); 
-     // afficher
-     //printf ("r=%d\tpin=%d\t",r,rowpin);
-     displayrow(d);   
-      
-     // reinitialiser la ligne a High
-     set_output(rowpin,1);
+    for (int r = 4; r>=1; r--){
+   
+       // Mettre la ligne a 0 
+        rowpin=ROW_1+(r-1)*2;
+        set_output(rowpin,0);
+        pause(500);
 
-  }      
+       // récupère les valeurs des colonnes
+       pinvalues = get_states(reed_end_Pin,reed_start_Pin);
+       //pause(1000);
+      // afficher
+//       printf ("row=%d\tpin=%d\tpinval=%u",r,rowpin,pinvalues);
+       printf ("Row_%d: ",r);
+       d = getcolpinval(pinvalues);
+       //printf("\td=%d\n",d); 
+       displayrow(d);   
+       printf("\n");
+       term_cmd(CRSRXY,TERM_X0,TERM_Y0 + 4-r+1);
+       // reinitialiser la ligne a High
+       set_output(rowpin,1);
+      // pause(500);
+
+    } // for row     
+  } // for(;;)
 }
